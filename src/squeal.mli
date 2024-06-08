ce25f808bbe4beadf960eea6b9027f55f036e20d
Tom Ekander <ekander.tom@gmail.com>
2024-06-08 20:00:39 +0200

    Not to be used

--- src/squeal.mli
+++ src/squeal.mli
@@ -10,38 +10,13 @@ end
 
 type t
 
-(** Create a value from an integer. *)
 val int : int -> Value.t
-
-(** Create a value from a float. *)
 val float : float -> Value.t
-
-(** Create a value from a string. *)
 val string : string -> Value.t
-
-(** Create a value from a boolean. *)
 val bool : bool -> Value.t
 
-(** [create query ~params] prepares a query for execution.
-
-    {[
-      let query =
-        Squeal.(create "select * from users where id = :id" ~params:[ ":id", string id ])
-      in
-
-      Database.exec query
-    ]} *)
 val create : string -> Value.t list -> t
 
-(** [to_string query] returns the SQL query as a string.
-
-    {[
-      Squeal.(
-        to_string
-        @@ prepare "select * from users where id = :id" ~params:[ ":id", int id ])
-
-      (* would yield "select * from users where id = 123" *)
-    ]} *)
 val to_string : t -> string
 
 type connection
